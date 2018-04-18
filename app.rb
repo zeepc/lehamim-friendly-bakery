6369125f66a95b6af7ecf480c01f306b2f68dc8a
zee <zperezcanals@gmail.com>
2018-04-18 14:17:58 -0400

    add address card to homepage

--- app.rb
+++ app.rb
@@ -1,19 +1,23 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require './main.rb'
 
 get '/' do 
 
  erb :index
- 
+
 end
 
 get '/:id' do
 
 	if params[:id] == "about"
 
+
 	erb :about
 
 elsif params[:id] == "menu"
+	# @forest_cake = Cake.new("Forest Fruits","Made with the finest chocolates from beligum, topped with fresh berries and house made jam. Perfect for any occasion!","$35")
+
 
 	erb :menu
 
--- main.rb
+++ main.rb
@@ -1,37 +1,33 @@
 class Cake
 	attr_accessor :cake, :description, :price
-	def intialize(cake,description,price)
-		@cake = cake
-		@description = description
-		@price = price
+	def intialize(cake)
+		@cake = {}
+		
 	end
 end
 
 class Cookie
 	attr_accessor :cookie, :description, :price
-	def intialize(cookie,description,price)
-		@cookie = cookie
-		@description =description
-		@price = price
+	def intialize(cookie)
+		@cookie = {}
+	
 	end
 end
 
 class Muffin
 	attr_accessor :muffin, :description, :price
-	def intialize(muffin,description,price)
-		@muffin = muffin
-		@description =description
-		@price = price
+	def intialize(muffin)
+		@muffin = {}
 	end
 end
 
 
-#Cake objects
-forest_cake = Cake.new("Forest Fruits","Made with the finest chocolates from beligum, topped with fresh berries and house made jam. Perfect for any occasion!","$35")
-babka = Cake.new("Chocolate Babka", "Swirly, old world goodness. The perfect companion for coffee or tea.", "$12")
+# #Cake objects
+# forest_cake = Cake.new("Forest Fruits","Made with the finest chocolates from beligum, topped with fresh berries and house made jam. Perfect for any occasion!","$35")
+# babka = Cake.new("Chocolate Babka", "Swirly, old world goodness. The perfect companion for coffee or tea.", "$12")
 
-#Cookie objects
-hamantaschen = Cookie.new()
-choc_chip = Cookie.new()
-multi = Cookie.new()
-#muffin object
+# #Cookie objects
+# hamantaschen = Cookie.new()
+# choc_chip = Cookie.new()
+# multi = Cookie.new()
+# #muffin object
