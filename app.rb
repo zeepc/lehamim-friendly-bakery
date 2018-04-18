b215977a91ed4e373d17ceaf25b8e79c67b9ca9e
zee <zperezcanals@gmail.com>
2018-04-17 20:45:37 -0400

    add slider images and functionality

---
+++ app.rb
@@ -0,0 +1,8 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do 
+
+ erb :index
+end
+
---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+ // var elem = document.querySelector('.slider');
+ // var instance = M.Slider.init(elem, options);
+
+
+$(document).ready(function(){
+    $('.slider').slider();
+  });
+
+// (function($){
+//   $(function(){
+
+//    $('.slider').slider();
+
+//   });
+// })(jQuery);
